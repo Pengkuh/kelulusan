26a8ca90c14cffbf72a2db89846bc8a62800acc8
Pengkuh <sultanpengkuh@gmail.com>
2025-01-21 22:34:25 +0700

    Pesan commit

--- model copy.py
+++ model copy.py
@@ -2,10 +2,15 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 
-# Membaca data dari file Excel
-df = pd.read_excel('data_kelulusan.xlsx')
+# Dataset contoh
+data = {
+    'Nilai_Matematika': [80, 70, 60, 85, 55],
+    'Nilai_Bahasa_Indonesia': [75, 65, 85, 80, 60],
+    'Nilai_Sains': [80, 60, 75, 90, 50],
+    'Status_Lulus': ['Lulus', 'Tidak Lulus', 'Lulus', 'Lulus', 'Tidak Lulus']
+}
 
-# Pisahkan fitur dan target
+df = pd.DataFrame(data)
 X = df.drop('Status_Lulus', axis=1)  # Fitur
 y = df['Status_Lulus']  # Target
 
